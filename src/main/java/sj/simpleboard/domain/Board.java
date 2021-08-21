44d95a9c0e623de39124393611246d50b92f183b
bae <baesj11@gmail.com>
2021-08-21 22:09:46 +0900

    class Board
    클래스 이름 수정

--- src/main/java/sj/simpleboard/domain/Board.java
+++ src/main/java/sj/simpleboard/domain/Board.java
@@ -3,16 +3,16 @@ package sj.simpleboard.domain;
 import lombok.Data;
 
 @Data
-public class board {
+public class Board {
     private Long no;
     private String title;
     private String contents;
     private String date;
 
-    public board() {
+    public Board() {
     }
 
-    public board(String title, String contents, String date) {
+    public Board(String title, String contents, String date) {
         this.title = title;
         this.contents = contents;
         this.date = date;
