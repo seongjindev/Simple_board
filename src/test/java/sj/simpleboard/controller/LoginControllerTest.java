df675f001b1df0fe638e6d515f073095e7013358
bae <baesj11@gmail.com>
2021-11-01 21:22:36 +0900

    TEST: 비밀번호 길이 제한 테스트
    .andExpect(model().errorCount(1))
     - 에러의 갯수 체크
    
    .andExpect(model().attributeHasFieldErrors("member", "memberPwd"))
     - 필드 에러가 있는지 확인 (member의 memberPwd)
    
    .andExpect(model().attributeHasFieldErrorCode("member", "memberPwd", "Range"))
     - 필드 에러 코드로 확인
     - member에서 memberPwd가 Range 에러인지 확인

--- src/test/java/sj/simpleboard/controller/LoginControllerTest.java
+++ src/test/java/sj/simpleboard/controller/LoginControllerTest.java
@@ -1,14 +1,21 @@
 package sj.simpleboard.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.validation.Errors;
+import sj.simpleboard.domain.Member;
+
+import javax.validation.Valid;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,6 +25,8 @@ class LoginControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    LoginController loginController;
 
     @Test
     void 로그인요청_확인() throws Exception {
@@ -25,5 +34,17 @@ class LoginControllerTest {
         mockMvc.perform(post("/login")
                 .param("memberId","aaafa").param("memberPwd", "faaa"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void 비밀번호_길이_제한() throws Exception {
+        //given //when //then
+        mockMvc.perform(post("/members/add")
+                .param("memberId", "testId")
+                .param("memberPwd", "test")
+                .param("memberNM", "test"))
+                .andExpect(model().errorCount(1))
+                .andExpect(model().attributeHasFieldErrors("member", "memberPwd"))
+                .andExpect(model().attributeHasFieldErrorCode("member", "memberPwd", "Range"));
     }
 }
